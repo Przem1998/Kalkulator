9ee5417c1ded0742158c7e2b4ddc8852bafc005f
USLove <USLove@users.noreply.github.com>
2020-02-25 12:32:08 +0100

    papka

---
+++ Kalkulator/stas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class stas
+    {
+
+        public stas()
+        {
+
+        }
+
+        public double podziel(double a,double b)
+        {
+            double result = a / b;
+            return result;
+        }
+        public double mnoz(double a,double b)
+        {
+            double result = a * b;
+            return result;
+        }
+    }
+}
