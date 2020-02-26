93aacdb3b2d26b32193339accec3a684c182c74b
Przem1998 <p.zeleznicki@protonmail.com>
2020-02-26 20:16:29 +0100

    added funtion adds and removes

---
+++ Kalkulator/AddRemove.cs
@@ -0,0 +1,15 @@
+﻿namespace Kalkulator
+{
+    public class AddRemove
+    {
+        public AddRemove() { }
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+        public double remove(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kalkulator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AddRemove activity = new AddRemove();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            double valueA = 0;
+            double valueB = 0;
+            try
+            {
+                valueA = double.Parse(aField.Text);
+                valueB = double.Parse(bField.Text);
+
+            }
+            catch (System.FormatException) { }
+  
+            resultActivity.Text = "="+activity.add(valueA, valueB);
+
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            double valueA = 0;
+            double valueB = 0;
+            try
+            {
+                valueA = double.Parse(aField.Text);
+                valueB = double.Parse(bField.Text);
+
+            }
+            catch (System.FormatException) { }
+
+            resultActivity.Text = "=" + activity.remove(valueA, valueB);
+        }
+
+        private void Multiple_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+     
+
+    }
+}
