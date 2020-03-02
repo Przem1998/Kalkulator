70fb67579b5f96a55f567e616509655631d20ffd
USLove <USLove@users.noreply.github.com>
2020-03-02 23:13:10 +0100

    kkkk

---
+++ Kalkulator/multiply_divide.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    class multiply_divide
+    {
+        public multiply_divide()
+        {
+
+        }
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
